35d7dcd72a77f6e49a85d966baa618574fc35b75
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2022-12-20 12:25:26 +0300

    Created unloading route

---
+++ routers.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter
+from typing import List
+
+from utils import (
+        get_data, unload, post_node, post_edge,
+        delete_node, delete_edge)
+from schemas import ResultBase, NodeBase, EdgeBase
+
+
+
+def file_name(file):
+    if file == "main":
+        return "static/data.json"
+    elif file == "node":
+        return  "static/node_data.txt"
+    elif file == "edge":
+        return "static/edge_data.txt"
+    else:
+        return {f"No file with the name { file }"}
+
+
+router = APIRouter()
+
+
+@router.get('/get/all', response_model=ResultBase)
+async def get_all():
+    """ Вывести все содержимое base.json"""
+    return get_data(fileName=file_name("main"))
+
+
+@router.get('/get/unloading', status_code=201)
+async def unloading():
+    """ Удалить помеченые для удаления и обновить информацию в base.json"""
+    unload(
+        edges=get_data(fileName=file_name("edge")), 
+        nodes=get_data(fileName=file_name("node")), 
+        main=get_data(fileName=file_name("main"))
+        )
+    return {"detail": "Created"}
+
+
+@router.post('/post/nodes')
+async def post_nodes(nodes: List[NodeBase]):
+    return post_node(nodes=nodes, fileName=file_name("node"))
+
+
+@router.post('/post/edges')
+async def post_edges(edges: List[EdgeBase]):
+    return post_edge(edges, fileName=file_name("edge"))
+
+
+@router.delete('/delete/nodes')
+async def delete_nodes(nodes: List[str]):
+    return delete_node(nodes, fileName=file_name("node"))
+
+
+@router.delete('/delete/edges')
+async def delete_edges(edges: List[str]):
+    return delete_edge(edges, fileName=file_name("edge"))
