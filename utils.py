69d75479638251700868af12524738939a92acea
AkimDuyshenaliev <akim.duyshenaliev@gmail.com>
2022-12-21 13:47:39 +0300

    Moved checks into it's own file

--- utils.py
+++ utils.py
@@ -1,49 +1,11 @@
-import os
 import json
 from fastapi import HTTPException
-
 from schemas import ResultBase
 
-
-
-def check_if_duplicate_key(old_data, new_data, fieldName, fileName):
-    """ Проверить если присланые ключи уже есть,
-    True - есть дубликаты в новых данных
-    False - нет дубликатов в новых данных"""
-
-    extract = lambda data : list(map(lambda x : x["key"], data))
-    old_keys = extract([json.loads(i) for i in old_data]) 
-    new_keys = extract(new_data)
-    for key in new_keys:
-        if key in old_keys:
-            return True
-    return False
-
-
-def check_if_duplicate_src_targ(new_data, old_data, fileName):
-    """ Проверить если присланные связи уже есть
-    True - есть дубликаты новых связей
-    False - нет дубликатов новых связей """
-
-    # Взять указаное "field" поле из "data" и записать в лист "x" и вернуть из lambda функции
-    # в переменную "extract"
-    extract = lambda data, field : list(map(lambda x : x[field], data))
-
-    temp_new_data = zip(
-            extract(new_data, "source"), 
-            extract(new_data, "target"))
-    temp_old_data = zip(
-            extract([json.loads(i) for i in old_data], "source"), 
-            extract([json.loads(i) for i in old_data], "target"))
-    temp_old_data_reverse = zip(
-            extract([json.loads(i) for i in old_data], "target"),
-            extract([json.loads(i) for i in old_data], "source")) 
-
-    for data in temp_new_data:
-        if data in temp_old_data or data in temp_old_data_reverse:
-            return True
-
-    return False
+from checks import (
+    check_if_duplicate_key, check_if_duplicate_src_targ,
+    check_if_empty
+    )
 
 
 def create_file(data, write_to, fileName):
@@ -105,17 +67,6 @@ def post_data(data, write_to, fileName):
             else:
                 base_file.write(json.dumps(ready_data) + "\n")
         return base_file
-    
-
-def check_if_empty(fileName):
-    """ Проверить что json пустой,
-    пустой - True
-    не пустой - False"""
-
-    # check if size of file is 0
-    if os.stat(fileName).st_size == 0:
-        return True
-    return False
     
 
 def delete(new_data, mainFile, fieldName, fileName):
